74b31e7546e153daf23f8d6e29dca28e759adadf
Yanglin Zhao <hi@yanglinzhao.com>
2026-01-24 21:36:08 -0500

    MORE PROGRESS

--- silakka54/keymaps/default/keymap.c
+++ silakka54/keymaps/default/keymap.c
@@ -27,19 +27,28 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base Layer
     [0] = LAYOUT(
-        KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,    KC_5,                     KC_6,  KC_7,   KC_8,    KC_9,   KC_0,      KC_MINS,
-        KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,                     KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,      KC_BSPC,
-        KC_LCTL, HOME_A, HOME_S, HOME_D, HOME_F,  KC_G,                     KC_H,  HOME_J, HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,                     KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,
-                                         KC_LGUI, MO(2), KC_SPC,    KC_ENT, MO(1), KC_RALT
+        KC_GRAVE, KC_1,   KC_2,   KC_3,   KC_4,    KC_5,                     KC_6,  KC_7,   KC_8,    KC_9,   KC_0,      KC_EQUAL,
+        KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,                     KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,      KC_AT,
+        KC_ESC,   HOME_A, HOME_S, HOME_D, HOME_F,  KC_G,                     KC_H,  HOME_J, HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
+        KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,                     KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,
+                                          KC_LGUI, MO(2), KC_SPC,    KC_ENT, MO(1), KC_RALT
     ),
 
     // Symbol Layer
     [1] = LAYOUT(
-        KC_TRNS, KC_F1,      KC_F2,    KC_F3,    KC_F4,    KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS, KC_GRAVE,   KC_TILDE, KC_HASH,  KC_AMPS,  KC_PIPE,                        KC_CARET, KC_LBRC, KC_RBRC, KC_LBKT,  KC_RBKT,  KC_TRNS,
-        KC_TRNS, KC_EXCL,    KC_UNDER, KC_COLON, KC_EQUAL, KC_DOLLAR,                      KC_AMPS, KC_LBAR, KC_RBAR,   KC_UNDER, KC_SEMI, KC_TRNS,
-        KC_TRNS, KC_PERCENT, KC_QMARK, KC_STAR,  KC_PLUS,  KC_BLSH,                        KC_FLSH, KC_MINUS, KC_LT, KC_GT, KC_DOUBLE_QUOTES, KC_TRNS,
-                                                 KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_F1,      KC_F2,    KC_F3,    KC_F4,    KC_F5,                          KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,           KC_F11,
+        KC_TRNS, KC_GRAVE,   KC_TILDE, KC_HASH,  KC_AMPS,  KC_PIPE,                        KC_CARET, KC_LBRC,  KC_RBRC, KC_LBKT,  KC_RBKT,          KC_TRNS,
+        KC_TRNS, KC_EXCL,    KC_UNDER, KC_COLON, KC_EQUAL, KC_DOLLAR,                      KC_AMPS,  KC_LBAR,  KC_RBAR, KC_UNDER, KC_SEMI,          KC_TRNS,
+        KC_TRNS, KC_PERCENT, KC_QMARK, KC_STAR,  KC_PLUS,  KC_BLSH,                        KC_FLSH,  KC_MINUS, KC_LT,   KC_GT,    KC_DOUBLE_QUOTES, KC_TRNS,
+                                                 KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS
     )
+
+    // Nav and Media Layer
+    [2] = LAYOUT(
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,                    KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS,      KC_MINS,
+        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                      KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,      KC_BSPC,
+        KC_TRNS,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSHIFT, KC_NO,                      KC_H,  HOME_J, HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
+        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                      KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,
+                                             KC_LGUI,   MO(2),   KC_SPC,    KC_ENT, MO(1), KC_RALT
+    ),
 };
