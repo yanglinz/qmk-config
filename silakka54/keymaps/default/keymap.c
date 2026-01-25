581669f1d56a99b152353fb044f04929ce4db5fa
Yanglin Zhao <hi@yanglinzhao.com>
2026-01-25 14:04:32 -0500

    finish setting up

--- silakka54/keymaps/default/keymap.c
+++ silakka54/keymaps/default/keymap.c
@@ -40,15 +40,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_GRV,     LSFT(KC_GRV),   KC_HASH,    LSFT(KC_7),     KC_PIPE,                        LSFT(KC_6), LSFT(KC_LBRC), LSFT(KC_RBRC),  KC_LBRC,        KC_RBRC,        KC_TRNS,
         KC_TRNS, LSFT(KC_1), LSFT(KC_MINUS), KC_COLON,   KC_EQUAL,       KC_DOLLAR,                      LSFT(KC_2), LSFT(KC_9),    LSFT(KC_0),     LSFT(KC_MINUS), KC_SCLN,        KC_TRNS,
         KC_TRNS, KC_PERCENT, LSFT(KC_SLASH), LSFT(KC_8), LSFT(KC_EQUAL), KC_BSLS,                        KC_SLSH,    KC_MINUS,      LSFT(KC_COMMA), LSFT(KC_DOT),   LSFT(KC_QUOTE), KC_TRNS,
-                                                         KC_TRNS,        KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS
+                                                         KC_TRNS,        MO(3),     KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS
     ),
 
     // Nav and Media Layer
     [2] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,                    KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                      KC_AGAIN, KC_PASTE, KC_COPY, KC_CUT,   KC_UNDO, KC_TRNS,
-        KC_TRNS,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT,   KC_NO,                      KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_CAPS, KC_TRNS,
-        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                      KC_HOME,  KC_PGDN,  KC_PGUP, KC_END,   KC_INS,  KC_TRNS,
-                                             KC_LGUI,   MO(2),   KC_SPC,    KC_ENT, MO(1),    KC_RALT
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, TT(4),
+        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_AGAIN, KC_PASTE, KC_COPY, KC_CUT,   KC_UNDO, KC_TRNS,
+        KC_TRNS,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_NO,                      KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_CAPS, KC_TRNS,
+        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_HOME,  KC_PGDN,  KC_PGUP, KC_END,   KC_INS,  KC_TRNS,
+                                             KC_LGUI, KC_TRNS, KC_SPC,    KC_ENT, MO(3),    KC_RALT
+    ),
+
+    // Number Layer
+    [3] = LAYOUT(
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                      KC_NO,   KC_7,    KC_8,    KC_9,    KC_UNDO, KC_TRNS,
+        KC_TRNS,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT,   KC_NO,                      KC_0,    KC_4,    KC_5,    KC_6,    KC_CAPS, KC_TRNS,
+        KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                      KC_NO,   KC_1,    KC_2,    KC_3,    KC_INS,  KC_TRNS,
+                                             KC_LGUI,   KC_TRNS,   KC_SPC,    KC_ENT, KC_TRNS, KC_RALT
+    ),
+
+    // Gaming Layer
+    [4] = LAYOUT(
+        KC_ESC,  KC_1, KC_2, KC_3, KC_4,   KC_5,              KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    TT(4),
+        KC_TAB,  KC_Q, KC_W, KC_E, KC_R,   KC_T               KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_TRNS,
+        KC_LSFT, KC_A, KC_S, KC_D, KC_F,   KC_G               KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z, KC_X, KC_C, KC_V,   KC_B               KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                                   KC_SPC, KC_SPC, KC_SPC,    KC_ENT, KC_TRNS, KC_RALT
     ),
 };
