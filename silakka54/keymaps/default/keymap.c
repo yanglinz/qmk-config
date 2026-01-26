05eb08cf66f0d020c0a2ba33c4a53e99a1def2dc
Yanglin Zhao <hi@yanglinzhao.com>
2026-01-25 21:29:45 -0500

    Use the windows default

--- silakka54/keymaps/default/keymap.c
+++ silakka54/keymaps/default/keymap.c
@@ -4,16 +4,16 @@
 #include QMK_KEYBOARD_H
 
 // Left-hand home row mods
-#define HOME_A LCTL_T(KC_A)
+#define HOME_A LGUI_T(KC_A)
 #define HOME_S LALT_T(KC_S)
-#define HOME_D LGUI_T(KC_D)
+#define HOME_D LCTL_T(KC_D)
 #define HOME_F LSFT_T(KC_F)
 
 // Right-hand home row mods
 #define HOME_J RSFT_T(KC_J)
-#define HOME_K RGUI_T(KC_K)
+#define HOME_K RCTL_T(KC_K)
 #define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RCTL_T(KC_SCLN)
+#define HOME_SCLN RGUI_T(KC_SCLN)
 
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     LAYOUT(
