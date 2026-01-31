c26241f8acc5dc5ae8334ca0f2a2b821a77e18e2
Yanglin Zhao <hi@yanglinzhao.com>
2026-01-30 22:08:51 -0500

    Use a dedicated moment layer button (#4)
    * reserve for mo
    
    * del

--- silakka54/config.h
+++ silakka54/config.h
@@ -18,9 +18,9 @@
 
 // home row mods (timerless)
 // https://www.reddit.com/r/ErgoMechKeyboards/comments/1q1jo3c/urobs_zmk_timeless_home_row_mods_ported_to_native/
-#define TAPPING_TERM 250
+#define TAPPING_TERM 270
 #define PERMISSIVE_HOLD
-#define FLOW_TAP_TERM 150
+#define FLOW_TAP_TERM 130
 #define CHORDAL_HOLD
 #define SPECULATIVE_HOLD
 
